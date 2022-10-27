1691d88cfdc4c9f35cecf588f674d97aa36c769f
krystalwilbert <krystalwilbert@ucsb.edu>
2022-10-27 15:35:45 -0700

    make crosstabs table

--- Experiment1_R_Analyses_Practice.R
+++ Experiment1_R_Analyses_Practice.R
@@ -1,11 +1,15 @@
+R.Version()
 # Install and load Packages
 
 install.packages("tidyverse")
 install.packages("ggplot2")
 install.packages("dplyr")
+install.packages("gmodels")
+
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(gmodels)
 
 # Assign the data to an object. We'll name our object "Experiment_1_Data"
 
@@ -39,35 +43,25 @@ View(Experiment_1_Data_Filtered)
 Tatt_in_Any <- Experiment_1_Data_Filtered %>%
   select(Tatt.in.Any)
 
-# Assign the Lineup.Condition variable to an object-- we'll call it "Tatt_in_Any"
+# Assign the Lineup.Condition variable to an object-- we'll call it "Lineup_Condition"
 
 Lineup_Condition <- Experiment_1_Data_Filtered %>%
   select(Lineup.Condition)
-
-# If you want to select two variables and make it into an object do the following: 
-
-Lineup_Condition_and_Tatt_in_any <- Experiment_1_Data_Filtered %>%
-  select(Lineup.Condition, Tatt.in.Any)
-
 
 # In order to view these datasets, use the function "View()"
 
 View(Tatt_in_Any)
 View(Lineup_Condition)
-View(Lineup_Condition_and_Tatt_in_any)
 
 # In order to make a crosstabs table, use the function...
 # ... table name <-  table(A,B)
 # A is rows, B is columns
 
-Tatt_in_Any_by_Lineup_Condition <- table(Experiment_1_Data_Filtered$Tatt.in.Any, Experiment_1_Data_Filtered$Lineup.Condition)
-
-
+Tatt_in_Any_by_Lineup_Condition <- table(Experiment_1_Data_Filtered$Tatt.in.Any, 
+                                         Experiment_1_Data_Filtered$Lineup.Condition)
 
 
 Tatt_in_Any_by_Lineup_Condition
-
-ftable(Tatt_in_Any_by_Lineup_Condition)
 
 barplot(Tatt_in_Any_by_Lineup_Condition, legend = TRUE, beside = TRUE, main = "Tatt in Any by Lineup Condition")
 
@@ -86,3 +80,42 @@ ftable(Tatt_in_Any_by_Describe_Before_by_Feedback)
 data.frame(Tatt_in_Any_by_Describe_Before_by_Feedback)
 
 
+
+
+
+## CrossTables
+require(gmodels)
+
+CrossTable(Experiment_1_Data_Filtered$Tatt.in.Any, 
+           Experiment_1_Data_Filtered$Lineup.Condition, digits = 3,
+           expected = TRUE, prop.r = TRUE, 
+           prop.c = TRUE, prop.t = TRUE, 
+           prop.chisq = TRUE, chisq = FALSE,
+           fisher = FALSE, mcnemar = FALSE, 
+           missing.include = FALSE)
+
+### digits: # of decimals
+### expected: shows expected frequencies
+### prop.r: row proportions
+### prop.c: column proportions
+### prop.chisq: chi square contributions
+### chisq: computes chi square test for association
+### fisher: Fisher exact test
+## mcnemar: computers the McNemar test (2x2 tables only)
+## missing.include: if TRUE removes unused factor levels
+
+## to show only observed counts, expected = FALSE, change prop.r = FALSE, prop.c = FALSE, prop.t = FALSE,
+## prop.chisq = FALSE, chisq = FALSE, fisher = FALSE, mcnemar = FALSE, missing.include = FALSE
+
+CrossTable(Experiment_1_Data_Filtered$Tatt.in.Any, Experiment_1_Data_Filtered$Lineup.Condition, digits = 3,
+           expected = FALSE, prop.r = FALSE, prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE, chisq = FALSE,
+           fisher = FALSE, mcnemar = FALSE, missing.include = FALSE)
+
+# Using xtabs
+
+Tatt_in_Any_by_Lineup_Condition <- xtabs(~ Tatt.in.Any + Lineup.Condition, data = Experiment_1_Data_Filtered)
+xtabs(~ Tatt.in.Any + Describe.Before + Feedback, data = Experiment_1_Data_Filtered)
+
+
+Tatt_in_Any_by_Lineup_Condition_Proportions <- prop.table(Tatt_in_Any_by_Lineup_Condition)
+Tatt_in_Any_by_Lineup_Condition_Proportions
